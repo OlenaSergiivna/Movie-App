a1cade51499a30e12a5dd1758c8b9b1708bf7812
Olena <elenasergeevvnna@gmail.com>
2022-09-26 01:31:45 +0300

    Created Session model. Added token validation (2/3 for authentication).

--- Movie App/Models/NetworkManager.swift
+++ Movie App/Models/NetworkManager.swift
@@ -12,16 +12,36 @@ struct NetworkManager {
     
     static let shared = NetworkManager()
     
-    func requestAutentification() {
-        
-        let apiKey = "b718f4e2921daaf000e347114cf44187"
-        
-        // MARK: - First: Request token
+    private let apiKey = "b718f4e2921daaf000e347114cf44187"
+    
+    // MARK: - First: Request token
+    
+    func requestAuthentication(completion: @escaping(String) -> Void) {
         
         let requestTokenUrl = "https://api.themoviedb.org/3/authentication/token/new?api_key=\(apiKey)"
         let tokenRequest = AF.request(requestTokenUrl, method: .get)
         
         tokenRequest.responseDecodable(of: Token.self) { response in
+            
+            do {
+                let data = try response.result.get().request_token
+                print(data)
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: - Second: Validate token
+    
+    func validateAuthentication(username: String, password: String, token: String) {
+        
+        let validateTokenUrl = "https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=\(apiKey)&username=\(username)&password=\(password)&request_token=\(token)"
+        
+        let tokenValidation = AF.request(validateTokenUrl, method: .post)
+        
+        tokenValidation.responseDecodable(of: Token.self) { response in
             
             do {
                 let data = try response.result.get()
@@ -31,7 +51,21 @@ struct NetworkManager {
             }
         }
         
+    }
+    
+    func createSession(token: String) {
         
+     let sessionUrl = "https://api.themoviedb.org/3/authentication/session/new?api_key=\(apiKey)&request_token=\(token)"
+        let creatingSession = AF.request(sessionUrl, method: .post)
         
+        creatingSession.responseDecodable(of: Session.self) { response in
+            
+            do {
+                let data = try response.result.get().session_id
+                print(data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
---
+++ Movie App/Models/SessionModel.swift
@@ -0,0 +1,13 @@
+//
+//  SessionModel.swift
+//  Movie App
+//
+//  Created by user on 26.09.2022.
+//
+
+import Foundation
+
+struct Session: Codable {
+    var success: Bool
+    var session_id: String
+}
