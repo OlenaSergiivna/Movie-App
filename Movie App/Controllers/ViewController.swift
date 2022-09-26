1340b30760636e5928ed6b5c6f544288d3606174
Olena <elenasergeevvnna@gmail.com>
2022-09-26 17:54:47 +0300

    Connected login and password textfields with authentication request. Added request in submit button.

--- Movie App/Controllers/ViewController.swift
+++ Movie App/Controllers/ViewController.swift
@@ -9,18 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sessionId = ""
+    
+    @IBOutlet weak var loginTextField: UITextField!
+
+    @IBOutlet weak var passTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NetworkManager.shared.requestAuthentication(username: "Olena.Olena", password: "olena1611") { id in
-            print("Session id (VC): \(id)")
+        resetForm()
+        
+
+    }
+
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        
+        guard let login = loginTextField.text else { return }
+        guard let password = passTextField.text else { return }
+
+        NetworkManager.shared.requestAuthentication(username: login, password: password) { [weak self] id in
+            self?.sessionId = id
+            print("Session id (VC): \(self?.sessionId ?? "0")")
            
         }
         
-    
-  
     }
+    
+    func resetForm() {
+        submitButton.isEnabled = true
 
+//        loginError.isHidden = false
+//        phoneError.isHidden = false
+//        passwordError.isHidden = false
 
+//        loginError.text = "Required"
+//        phoneError.text = "Required"
+//        passwordError.text = "Required"
+
+        loginTextField.text = ""
+        passTextField.text = ""
+    }
+    
+    
 }
 
