773843389e53d59dfef6bdd9b48f288294baa0f6
Olena <elenasergeevvnna@gmail.com>
2022-10-23 22:24:18 +0300

    Added Lottie framework. Created AnimationService, created animation function that added animation on the main screen.

--- Movie App/Controllers/AuthenticationViewController.swift
+++ Movie App/Controllers/AuthenticationViewController.swift
@@ -26,6 +26,7 @@ class AuthenticationViewController: UIViewController {
         super.viewDidLoad()
         
         resetForm()
+        AnimationService.shared.addAnimation(view: view)
         
     }
     
---
+++ Movie App/Managers/AnimationsService.swift
@@ -0,0 +1,28 @@
+//
+//  AnimationsService.swift
+//  Movie App
+//
+//  Created by user on 23.10.2022.
+//
+
+import UIKit
+import Lottie
+
+struct AnimationService {
+    
+    static let shared = AnimationService()
+    
+    func addAnimation(view: UIView) {
+        
+        let animation = AnimationView(name: "movie")
+        animation.frame = CGRect(x: 0, y: 0, width: view.frame.width * 0.5, height: view.frame.width * 0.5)
+        animation.center = CGPoint(x: view.frame.midX + view.frame.midX / 6, y: view.frame.midY - 300)
+        animation.loopMode = .repeat(1)
+        view.addSubview(animation)
+        
+        animation.play { finished in
+            print("animation finished")
+            
+        }
+    }
+}
