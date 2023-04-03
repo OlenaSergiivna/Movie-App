de5d624c27e187b2d64ca1f1ac1c8a6edda4d81e
Olena <elenasergeevvnna@gmail.com>
2023-04-03 03:47:05 +0300

    Added duck animation, app icon, app display name.

--- Movie App/Controllers/GetStartedViewController.swift
+++ Movie App/Controllers/GetStartedViewController.swift
@@ -6,33 +6,58 @@
 //
 
 import UIKit
+import AVKit
 
 class GetStartedViewController: UIViewController {
     
+    var playerLayer: AVPlayerLayer!
+    
+    var player: AVPlayer!
+    
     deinit {
         print("!!! Deinit: \(self)")
-      }
-
+    }
+    
     @IBOutlet weak var bottomView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureAnimation()
+    }
+    
+    func configureAnimation() {
+        guard let videoURL = Bundle.main.url(forResource: "001", withExtension: "mp4") else { return }
+        
+        player = AVPlayer(url: videoURL)
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        
+        view.layer.addSublayer(playerLayer)
+        player.play()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+    }
+    
+    @objc func playerDidFinishPlaying() {
+        playerLayer.removeFromSuperlayer()
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-    
+        
         navigationController?.navigationBar.isHidden = true
     }
-
+    
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         bottomView.layer.cornerRadius = 10
         bottomView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-        
     }
     
     
@@ -41,9 +66,9 @@ class GetStartedViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         guard let destinationViewController = storyboard.instantiateViewController(withIdentifier: "AuthenticationViewController") as? AuthenticationViewController else { return }
-
+        
         self.present(destinationViewController, animated: true)
-
+        
     }
     
     
@@ -54,14 +79,13 @@ class GetStartedViewController: UIViewController {
             guard data.success else { return }
             
             UserDefaultsManager.shared.saveUsersDataInUserDefaults(sesssionID: data.guest_session_id, isGuestSession: true, sessionExpireDate: data.expires_at)
-          
+            
             guard UserDefaults.standard.bool(forKey: UserDefaultsManager.shared.getKeyFor(.isGuestSession)) == true else { return }
             
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
             
             (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController, animated: true)
-            
         }
     }
 }
